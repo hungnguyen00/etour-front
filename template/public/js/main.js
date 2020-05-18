366e1b2e14283d813523c0b903d2a8399d8bbb15
Huong Nguyen <huongnguyen89.sp@gmail.com>
2020-05-19 01:52:03 +0700

    Add layout top

--- template/public/js/main.js
+++ template/public/js/main.js
@@ -11,10 +11,10 @@ function sliderWorks(name) {
 
 function fixHeader() {
   $(window).scroll(function() {
-    if($(window).scrollTop() <= 60) {
-      $('#header').removeClass('scroll');
+    if($(window).scrollTop() <= 20) {
+      $('.header').removeClass('scroll');
     } else {
-      $('#header').addClass('scroll');
+      $('.header').addClass('scroll');
     }
   });
 }
@@ -67,12 +67,12 @@ $(document).ready(function(){
 
     // Menu sp open
     var navIcon = $('.nav_icon');
-    var show_menu = $('.show_menu');
+    var show_menu = $('.menu-nav');
     navIcon.click(function(){
       $(this).toggleClass('nav_icon_open');
       $('body').toggleClass('show_menu');
-      $('body').find('.section_menu').toggleClass('section_menu_show');
-        show_menu.fadeToggle('400', function() {
+      //$('body').find('.section_menu').toggleClass('section_menu_show');
+      show_menu.fadeToggle('400', function() {
       });
     });
 		
